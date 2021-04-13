00bb89b97667031014d19a29885ad5916ec0f326
javroi <javroi@alu.iesserpis.org>
2021-04-13 11:11:45 +0200

    Creada interfaz basica del proyecto

---
+++ nexus_of_gamers/src/main/java/com/tfc/javierros/modelo/Usuario.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tfc.javierros.modelo;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author javie
+ */
+@Entity
+@Table (name="user")
+public class Usuario implements Serializable{
+    private static final long SerialVersionUID=1L;
+    @Id
+    private int id_user;
+    private String name;
+    private String surname;
+    private String nickname;
+    private String email;
+    private String password;
+    private int id_game_list;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id_user) {
+        this.id_user = id_user;
+    }
+
+    public Usuario(int id_user, String name, String surname, String nickname, String email, String password) {
+        this.id_user = id_user;
+        this.name = name;
+        this.surname = surname;
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Usuario(int id_user, String name, String surname, String nickname, String email, String password, int id_game_list) {
+        this.id_user = id_user;
+        this.name = name;
+        this.surname = surname;
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.id_game_list = id_game_list;
+    }
+
+    public int getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId_game_list() {
+        return id_game_list;
+    }
+
+    public void setId_game_list(int id_game_list) {
+        this.id_game_list = id_game_list;
+    }
+    
+    
+}
---
+++ nexus_of_gamers/src/main/java/com/tfc/javierros/servlets/ControllerUsuario.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tfc.javierros.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("/ControllerUsuario")
+public class ControllerUsuario extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String op = request.getParameter("op");
+        String url = "";
+        switch (op) {
+        }
+    }
+}
